1e0856fd88486c6bf65ed5495566b5cb014e1bd3
olaooch <fakoredesunday61@gmail.com>
2022-02-19 12:43:42 +0100

    new commit

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import Footer from './Footer'
+
+function App(){
+
+    return(
+
+        <div>
+           <Footer />
+           Hello, am the factor
+           <Footer />
+           you
+           <Footer />
+           <Footer />
+
+        </div>
+
+    )
+}
+
+export default Footer;
